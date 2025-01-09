c0dce4c936262ecbf24820ac5c07acea4f1505ec
Daniel Wrench <daniel@harmonic.co.nz>
2025-01-10 12:00:03 +1300

    making multi-distribution plotting generic (e.g. to corr scale)

--- slope_plots_and_tests.py
+++ slope_plots_and_tests.py
@@ -1,6 +1,8 @@
-# SLOPE HISTOGRAM PLOT
-# This script creates a histogram of the slopes of the regression lines fitted to the log-log structure functions.
+# MULTI-DISTRIBUTION PLOT
+# This script plots the distributions of the slopes, correlation scales, etc., for the different gap handling methods
+# Inspired by Fig. 6 (Taylor scales) in Reynolds paper
 
+# For publication, perhaps add asterisks to indicate significance of difference of means
 
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -15,17 +17,18 @@ plt.rcParams["ytick.direction"] = "in"
 
 # Load data
 ints = pd.read_csv("gapped_ints_metadata.csv")
+variable = "tce"  # Change this to the variable you want to analyze
 
-slopes_naive = ints[ints.gap_handling == "naive"].slope
-slopes_lint = ints[ints.gap_handling == "lint"].slope
-slopes_corrected = ints[ints.gap_handling == "corrected_3d"].slope
-slopes_true = ints[ints.gap_handling == "naive"].slope_orig
+data_naive = ints[ints.gap_handling == "naive"][variable]
+data_lint = ints[ints.gap_handling == "lint"][variable]
+data_corrected = ints[ints.gap_handling == "corrected_3d"][variable]
+data_true = ints[ints.gap_handling == "naive"][f"{variable}_orig"]
 
 # Perform a Wilcoxon signed-rank test to see if the means are significantly different from 2/3
 print(
-    "Results of Wilcoxon signed-rank test for difference of mean of True distribution from 2/3:"
+    f"Results of Wilcoxon signed-rank test for difference of mean of True distribution from 2/3 for {variable}:"
 )
-print(wilcoxon(slopes_true - 2 / 3))
+print(wilcoxon(data_true - 2 / 3))
 # WilcoxonResult(statistic=765975.0, pvalue=0.0)
 
 
@@ -39,69 +42,74 @@ def psd_to_sf(x):
 
 
 # Create the KDE plot for each distribution
-fig, ax = plt.subplots(figsize=(3.5, 2))
+fig, ax = plt.subplots(figsize=(5, 2.5))
 sns.kdeplot(
-    slopes_true,
-    label=f"True ({slopes_true.mean():.3f})",
+    data_true,
+    label=f"True ({data_true.mean():.3f})",
     color="grey",
     lw=2,
     ax=ax,
 )
 sns.kdeplot(
-    slopes_corrected,
-    label=f"Corrected ({slopes_corrected.mean():.3f})",
+    data_corrected,
+    label=f"Corrected ({data_corrected.mean():.3f})",
     color="#1b9e77",
     linestyle="dotted",
     ax=ax,
 )
 sns.kdeplot(
-    slopes_naive,
-    label=f"Naive ({slopes_naive.mean():.3f})*",
+    data_naive,
+    label=f"Naive ({data_naive.mean():.3f})",
     color="indianred",
     linestyle="dashed",
     ax=ax,
 )
 sns.kdeplot(
-    slopes_lint,
-    label=f"LINT ({slopes_lint.mean():.3f})*",
+    data_lint,
+    label=f"LINT ({data_lint.mean():.3f})",
     color="black",
     linestyle="dashdot",
     ax=ax,
 )
 
-# Add labels and title
-plt.xlabel("Slope of fitted regression line to log-log SF")
-
-# Create a secondary x-axis that is the negative of the original x-axis
-secax = ax.secondary_xaxis("top", functions=(sf_to_psd, psd_to_sf))
-secax.set_xlabel("Equivalent spectral index")
-
 # Add vertical lines at the means of the distributions
-plt.axvline(slopes_naive.mean(), color="indianred", linestyle="dashed", lw=0.7)
-plt.axvline(slopes_lint.mean(), color="black", linestyle="dashdot", lw=0.7)
-plt.axvline(slopes_corrected.mean(), color="#1b9e77", linestyle="dotted", lw=0.7)
-plt.axvline(slopes_true.mean(), color="grey", linestyle="solid", lw=0.7)
+plt.axvline(data_naive.mean(), color="indianred", linestyle="dashed", lw=0.7)
+plt.axvline(data_lint.mean(), color="black", linestyle="dashdot", lw=0.7)
+plt.axvline(data_corrected.mean(), color="#1b9e77", linestyle="dotted", lw=0.7)
+plt.axvline(data_true.mean(), color="grey", linestyle="solid", lw=0.7)
 
-# Add vertical line at 2/3
-plt.axvline(2 / 3, color="mediumblue", linestyle="solid", alpha=0.3, lw=0.5)
-# Annotate next to this line
-plt.text(
-    2 / 3 + 0.01,
-    5,
-    "K41 prediction",
-    va="center",
-    ha="left",
-    fontsize=9,
-    color="mediumblue",
-    alpha=0.5,
-)
-plt.legend(loc="upper center", bbox_to_anchor=(0.5, -0.25), ncol=2)
+# plt.legend(loc="upper center", bbox_to_anchor=(0.5, -0.25), ncol=2)
+plt.legend()
 plt.yticks([])
-plt.semilogy()
-plt.ylim(1e-2, 1e1)
-plt.xlim(-0.15, 1.25)
-plt.savefig("plots/results/final/slope_hist_log.pdf", bbox_inches="tight")
-# plt.show()
+
+if variable == "slope":
+    # Add labels and title
+    plt.xlabel("slope of fitted regression line to log-log SF")
+
+    # Create a secondary x-axis that is the negative of the original x-axis
+    secax = ax.secondary_xaxis("top", functions=(sf_to_psd, psd_to_sf))
+    secax.set_xlabel("Equivalent spectral index")
+
+    # Add vertical line at 2/3
+    plt.axvline(2 / 3, color="mediumblue", linestyle="solid", alpha=0.3, lw=0.5)
+    # Annotate next to this line
+    plt.text(
+        2 / 3 + 0.01,
+        5,
+        "K41 prediction",
+        va="center",
+        ha="left",
+        fontsize=9,
+        color="mediumblue",
+        alpha=0.5,
+    )
+
+    plt.semilogy()
+    plt.ylim(1e-2, 1e1)
+    plt.xlim(-0.15, 1.25)
+
+# plt.savefig("plots/results/final/slopes_hist_log.pdf", bbox_inches="tight")
+plt.show()
 
 # STATISTICAL TEST OF DIFFERENCE OF MEANS
 # Perform an ANOVA test to see if the means are significantly different?
@@ -109,45 +117,47 @@ plt.savefig("plots/results/final/slope_hist_log.pdf", bbox_inches="tight")
 # First, testing assumptions of ANOVA
 
 # 1. Homogeneity of variances
-print("\nResults of Levene's test for homogeneity of variances:")
-print(levene(slopes_naive, slopes_lint, slopes_corrected, slopes_true))
+print(f"\nResults of Levene's test for homogeneity of variances for {variable}:")
+print(levene(data_naive, data_lint, data_corrected, data_true))
 # If the p-value is less than 0.05, then the variances are significantly different
-print("HOMOGENEITY OF VARIANCES NOT SATISFIED")
+# print("HOMOGENEITY OF VARIANCES NOT SATISFIED")
 
 # 2. Normality
-print("\nResults of Shapiro-Wilk test for normality:")
-print(shapiro(slopes_naive))
-print(shapiro(slopes_lint))
-print(shapiro(slopes_corrected))
-print(shapiro(slopes_true))
-print("NORMALITY NOT SATISFIED")
+print(f"\nResults of Shapiro-Wilk test for normality for {variable}:")
+print(shapiro(data_naive))
+print(shapiro(data_lint))
+print(shapiro(data_corrected))
+print(shapiro(data_true))
+# print("NORMALITY NOT SATISFIED")
 # If the p-value is less than 0.05, then the data is not normally distributed
-print("\nCANNOT PROCEED WITH ANOVA")
+# print("\nCANNOT PROCEED WITH ANOVA")
 
 # Combine all the data into one array
 all_data = pd.DataFrame(
     {
-        "slope": pd.concat([slopes_naive, slopes_lint, slopes_corrected, slopes_true]),
-        "group": ["naive"] * len(slopes_naive)
-        + ["lint"] * len(slopes_lint)
-        + ["corrected"] * len(slopes_corrected)
-        + ["orig"] * len(slopes_true),
+        variable: pd.concat([data_naive, data_lint, data_corrected, data_true]),
+        "group": ["naive"] * len(data_naive)
+        + ["lint"] * len(data_lint)
+        + ["corrected"] * len(data_corrected)
+        + ["orig"] * len(data_true),
     }
 )
 
 # Since we can't perform ANOVA, instead use non-parametric test for difference of means
 # H0: The means of the groups are equal
 # H1: At least one of the means is different
-print("\nResults of non-parametric Kruskal-Wallis test for difference of means:")
-print(kruskal(slopes_naive, slopes_lint, slopes_corrected, slopes_true))
-print("REJECT NULL HYPOTHESIS OF EQUAL MEANS")
+print(
+    f"\nResults of non-parametric Kruskal-Wallis test for difference of means for {variable}:"
+)
+print(kruskal(data_naive, data_lint, data_corrected, data_true))
+# print("REJECT NULL HYPOTHESIS OF EQUAL MEANS")
 
 # If the Kruskal-Wallis test is significant, then perform a post-hoc test to see which groups are different
-print("\nResults of post-hoc Dunn's test for difference of means:")
+print(f"\nResults of post-hoc Dunn's test for difference of means for {variable}:")
 print(
     posthoc_dunn(
         all_data,
-        val_col="slope",
+        val_col=variable,
         group_col="group",
     )
 )
@@ -191,7 +201,7 @@ print(
 #     return bins, normhist
 
 
-# # Create histograms for all the different slopes
+# # Create histograms for all the different slope
 # bins_naive, hist_naive = normhist(slopes_naive.values, nbins=15)
 # bins_lint, hist_lint = normhist(slopes_lint.values, nbins=15)
 # bins_corrected, hist_corrected = normhist(slopes_corrected.values, nbins=15)
@@ -205,7 +215,7 @@ print(
 # plt.plot(bins_lint, hist_lint, label="LINT", color="black")
 
 # # Add labels and title
-# plt.xlabel("Slope of fitted regression line to log-log SF")
+# plt.xlabel("slope of fitted regression line to log-log SF")
 # plt.ylabel("Density")
 # plt.legend()
 # plt.semilogy()

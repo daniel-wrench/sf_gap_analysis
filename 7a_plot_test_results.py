eaf9430769dc06eef8ac01a3f638c26d7033d0e7
Daniel Wrench <daniel@harmonic.co.nz>
2025-01-26 13:52:45 +1300

    adding scatterplots for all derived stats, updating README

--- 7a_plot_test_results.py
+++ 7a_plot_test_results.py
@@ -39,8 +39,8 @@ ints_metadata = data["ints_metadata"]
 # ints = data["ints"]
 ints_gapped_metadata = data["ints_gapped_metadata"]
 # Export as csv
-ints_gapped_metadata.to_csv("ints_gapped_metadata.csv")
-files_metadata.to_csv("files_metadata.csv")
+ints_gapped_metadata.to_csv("ints_gapped_metadata.csv", index=False)
+files_metadata.to_csv("files_metadata.csv", index=False)
 
 
 # ints_gapped = data["ints_gapped"]
@@ -105,12 +105,16 @@ plt.tight_layout()
 
 plt.suptitle("")  # Remove the default title to avoid overlap
 plt.savefig(
-    f"plots/results/{output_path}/test_{spacecraft}_boxplots_{n_bins}_bins.pdf",
+    f"plots/results/{output_path}/test_{spacecraft}_boxplots_{n_bins}_bins_NEW.pdf",
     bbox_inches="tight",
 )
 
+
+# -------------------------------------------------------------------------------------#
+
 # Regression lines
-# Paper figures, so removing sub-par 2D correction
+
+# (Not including sub-par 2D corrected results)
 
 custom_order = ["naive", "lint", "corrected_3d"]
 colors = ["indianred", "dimgrey", "#1b9e77"]
@@ -121,152 +125,220 @@ palette = dict(zip(custom_order, colors))
 # unique_gap_handling = ints_gapped_metadata["gap_handling"].unique()
 # unique_gap_handling = ["naive", "lint", "corrected_3d"]
 
-# Plotting the MAPE vs. missing percentage
-fig, ax = plt.subplots(
-    1,
-    len(custom_order) + 1,
-    figsize=(7, 2),
-    sharex="col",
-    sharey="row",
-    tight_layout=True,
-)
-plt.subplots_adjust(wspace=0)
-
-# Add regression lines for each group
-
-for i, gap_handling_method in enumerate(custom_order):
-    subset = ints_gapped_metadata[
-        ints_gapped_metadata["gap_handling"] == gap_handling_method
-    ]
-    sns.scatterplot(
-        data=subset,
-        x="missing_percent_overall",
-        y="mape",
-        alpha=0.1,
-        s=10,
-        color=palette[gap_handling_method],
-        label=gap_handling_method,
-        ax=ax[i],
-        legend=False,
+for error_metric in ["mape", "slope_ape", "tce_ape", "ttu_ape"]:
+    fig, ax = plt.subplots(
+        1,
+        len(custom_order) + 1,
+        figsize=(7, 2),
+        sharex="col",
+        sharey="row",
+        tight_layout=True,
     )
+    plt.subplots_adjust(wspace=0)
 
-    # Create a secondary axis for the boxplot on the right-hand spine
-    ax_right = ax[i].inset_axes([1, 0, 0.1, 1], sharey=ax[i])
-    sns.boxplot(
-        data=subset,
-        y="mape",
-        ax=ax_right,
-        orient="v",
-        whis=(0, 100),
-        color=palette[gap_handling_method],
-        linecolor="black",
-        linewidth=1.2,
+    # Add regression lines for each group
+
+    for i, gap_handling_method in enumerate(custom_order):
+        subset = ints_gapped_metadata[
+            ints_gapped_metadata["gap_handling"] == gap_handling_method
+        ]
+        sns.scatterplot(
+            data=subset,
+            x="missing_percent_overall",
+            y=error_metric,
+            alpha=0.1,
+            s=10,
+            color=palette[gap_handling_method],
+            label=gap_handling_method,
+            ax=ax[i],
+            legend=False,
+        )
+
+        sns.regplot(
+            data=subset,
+            x="missing_percent_overall",
+            y=error_metric,
+            scatter=False,
+            color=palette[gap_handling_method],
+            label=gap_handling_method,
+            order=2,
+            ax=ax[-1],
+            ci=99,
+            line_kws={"linewidth": 0.8},  # Set the line width to be thinner
+        )
+
+    # Move titles to inside top each plot
+    for i, title in enumerate(["Naive", "LINT", "Corrected", "Regression lines"]):
+        ax[i].text(
+            0.5,
+            0.98,
+            title,
+            transform=ax[i].transAxes,
+            verticalalignment="top",
+            horizontalalignment="center",
+        )
+
+    ax[0].set(xlabel="", ylabel=f"{error_metric} (\%)")
+    ax[1].set(xlabel="", ylabel="")
+    ax[2].set(xlabel="", ylabel="")
+    ax[3].set(xlabel="", ylabel="")
+
+    # Remove gridlines and plot outlines
+
+    # Make one x-axis label for all plots
+    fig.text(0.5, 0.00, "TGP (\%)", ha="center", va="center")
+
+    for j in range(4):
+        ax[j].grid(False)
+        ax[j].set_xticks([0, 25, 50, 75, 100])
+        ax[j].set_xlim(-15, 105)
+        ax[j].spines["top"].set_visible(False)
+        ax[j].spines["right"].set_visible(False)
+
+    # Plotting the MAPE vs. missing percentage
+    fig, ax = plt.subplots(
+        1,
+        len(custom_order) + 1,
+        figsize=(7, 2),
+        sharex="col",
+        sharey="row",
+        tight_layout=True,
     )
+    plt.subplots_adjust(wspace=0)
 
-    # Hide the y-axis labels of the boxplot to avoid duplication
-    ax_right.yaxis.set_visible(False)
+    # Add regression lines for each group
 
-    # # Optional: Remove the x-axis labels and ticks of the boxplot
-    # ax_right.set_xticks([])
-    # ax_right.set_xlabel("")
+    for i, gap_handling_method in enumerate(custom_order):
+        subset = ints_gapped_metadata[
+            ints_gapped_metadata["gap_handling"] == gap_handling_method
+        ]
+        sns.scatterplot(
+            data=subset,
+            x="missing_percent_overall",
+            y=error_metric,
+            alpha=0.1,
+            s=10,
+            color=palette[gap_handling_method],
+            label=gap_handling_method,
+            ax=ax[i],
+            legend=False,
+        )
 
-    ax_right.spines["left"].set_visible(False)
-    ax_right.spines["right"].set_visible(False)
-    ax_right.spines["top"].set_visible(False)
+        # Create a secondary axis for the boxplot on the right-hand spine
+        ax_right = ax[i].inset_axes([1, 0, 0.1, 1], sharey=ax[i])
+        sns.boxplot(
+            data=subset,
+            y=error_metric,
+            ax=ax_right,
+            orient="v",
+            whis=(0, 100),
+            color=palette[gap_handling_method],
+            linecolor="black",
+            linewidth=1.2,
+        )
 
-    sns.regplot(
-        data=subset,
-        x="missing_percent_overall",
-        y="mape",
-        scatter=False,
-        color=palette[gap_handling_method],
-        label=gap_handling_method,
-        order=2,
-        ax=ax[-1],
-        ci=99,
-        line_kws={"linewidth": 0.8},  # Set the line width to be thinner
+        # Hide the y-axis labels of the boxplot to avoid duplication
+        ax_right.yaxis.set_visible(False)
+
+        # # Optional: Remove the x-axis labels and ticks of the boxplot
+        # ax_right.set_xticks([])
+        # ax_right.set_xlabel("")
+
+        ax_right.spines["left"].set_visible(False)
+        ax_right.spines["right"].set_visible(False)
+        ax_right.spines["top"].set_visible(False)
+
+        sns.regplot(
+            data=subset,
+            x="missing_percent_overall",
+            y=error_metric,
+            scatter=False,
+            color=palette[gap_handling_method],
+            label=gap_handling_method,
+            order=2,
+            ax=ax[-1],
+            ci=99,
+            line_kws={"linewidth": 0.8},  # Set the line width to be thinner
+        )
+
+        # sns.scatterplot(
+        #     data=subset,
+        #     x="missing_percent_overall",
+        #     y="slope_ape",
+        #     alpha=0.3,
+        #     s=10,
+        #     color=palette[gap_handling_method],
+        #     label=gap_handling_method,
+        #     ax=ax[1, i],
+        #     legend=False,
+        # )
+
+        # sns.regplot(
+        #     data=subset,
+        #     x="missing_percent_overall",
+        #     y="slope_ape",
+        #     scatter=False,
+        #     color=palette[gap_handling_method],
+        #     label=gap_handling_method,
+        #     order=2,
+        #     ax=ax[1, -1],
+        #     ci=99,
+        # )
+
+    # Move titles to inside top each plot
+    for i, title in enumerate(["Naive", "LINT", "Corrected", "Regression lines"]):
+        ax[i].text(
+            0.5,
+            0.98,
+            title,
+            transform=ax[i].transAxes,
+            verticalalignment="top",
+            horizontalalignment="center",
+        )
+
+    ax[0].set(xlabel="", ylabel=f"{error_metric} (\%)")
+    ax[1].set(xlabel="", ylabel="")
+    ax[2].set(xlabel="", ylabel="")
+    ax[3].set(xlabel="", ylabel="")
+
+    # ax[1, 0].set(xlabel="", ylabel="Slope APE (\%)", title="")
+    # ax[1, 1].set(xlabel="", ylabel="", title="")
+    # ax[1, 2].set(xlabel="", ylabel="", title="")
+    # ax[1, 3].set(xlabel="", ylabel="", title="")
+    # Remove gridlines and plot outlines
+
+    # Make one x-axis label for all plots
+    fig.text(0.5, 0.00, "TGP (\%)", ha="center", va="center")
+
+    # for i in range(2):
+    for j in range(4):
+        ax[j].grid(False)
+        ax[j].set_xticks([0, 25, 50, 75, 100])
+        ax[j].set_xlim(-15, 105)
+        ax[j].spines["top"].set_visible(False)
+        # ax[j].spines["left"].set_visible(False)
+        ax[j].spines["right"].set_visible(False)
+
+        ax[j].set_ylim(0, 60)
+        ax[j].set_xlim(-15, 105)
+        ax[j].set_ylim(0, 150)
+
+    # Remove ticks from all but first column
+    # for i in range(1, 4):
+    #     ax[i].set_yticks([])
+    #     ax[1, i].set_yticks([])
+
+    ax[0].spines["left"].set_visible(True)
+    # ax[1, 0].spines["left"].set_visible(True)
+
+    # Add title
+    # plt.suptitle(f"Error vs. \% missing data for the Wind test set ({n_bins} bins)")
+    # plt.show()
+    plt.savefig(
+        f"plots/results/{output_path}/test_{spacecraft}_scatterplots_{n_bins}_bins_{error_metric}_NEW.png",
+        bbox_inches="tight",
+        dpi=300,
     )
-
-    # sns.scatterplot(
-    #     data=subset,
-    #     x="missing_percent_overall",
-    #     y="slope_ape",
-    #     alpha=0.3,
-    #     s=10,
-    #     color=palette[gap_handling_method],
-    #     label=gap_handling_method,
-    #     ax=ax[1, i],
-    #     legend=False,
-    # )
-
-    # sns.regplot(
-    #     data=subset,
-    #     x="missing_percent_overall",
-    #     y="slope_ape",
-    #     scatter=False,
-    #     color=palette[gap_handling_method],
-    #     label=gap_handling_method,
-    #     order=2,
-    #     ax=ax[1, -1],
-    #     ci=99,
-    # )
-
-
-# Move titles to inside top each plot
-for i, title in enumerate(["Naive", "LINT", "Corrected", "Regression lines"]):
-    ax[i].text(
-        0.5,
-        0.98,
-        title,
-        transform=ax[i].transAxes,
-        verticalalignment="top",
-        horizontalalignment="center",
-    )
-
-ax[0].set(xlabel="", ylabel="MAPE (\%)")
-ax[1].set(xlabel="", ylabel="")
-ax[2].set(xlabel="", ylabel="")
-ax[3].set(xlabel="", ylabel="")
-
-# ax[1, 0].set(xlabel="", ylabel="Slope APE (\%)", title="")
-# ax[1, 1].set(xlabel="", ylabel="", title="")
-# ax[1, 2].set(xlabel="", ylabel="", title="")
-# ax[1, 3].set(xlabel="", ylabel="", title="")
-# Remove gridlines and plot outlines
-
-# Make one x-axis label for all plots
-fig.text(0.5, 0.00, "TGP (\%)", ha="center", va="center")
-
-# for i in range(2):
-for j in range(4):
-    ax[j].grid(False)
-    ax[j].set_xticks([0, 25, 50, 75, 100])
-    ax[j].set_xlim(-15, 105)
-    ax[j].spines["top"].set_visible(False)
-    # ax[j].spines["left"].set_visible(False)
-    ax[j].spines["right"].set_visible(False)
-
-    ax[j].set_ylim(0, 60)
-    ax[j].set_xlim(-15, 105)
-    ax[j].set_ylim(0, 150)
-
-# Remove ticks from all but first column
-# for i in range(1, 4):
-#     ax[i].set_yticks([])
-#     ax[1, i].set_yticks([])
-
-
-ax[0].spines["left"].set_visible(True)
-# ax[1, 0].spines["left"].set_visible(True)
-
-# Add title
-# plt.suptitle(f"Error vs. \% missing data for the Wind test set ({n_bins} bins)")
-# plt.show()
-plt.savefig(
-    f"plots/results/{output_path}/test_{spacecraft}_scatterplots_{n_bins}_bins.png",
-    bbox_inches="tight",
-    dpi=300,
-)
 
 # Error trendlines (REQUIRE FULL CORRECTED SFS, NOT CURRENTLY OUTPUT FROM HPC)
 
